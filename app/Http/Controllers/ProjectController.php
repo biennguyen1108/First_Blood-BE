2dd185fb2f85a6168aaa9760b77086eb168578ef
wuaann <114000299+wuaann@users.noreply.github.com>
2023-07-03 10:08:34 +0700

    m

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ProjectRequest;
 use App\Models\Project;
+use App\Models\ProjectUser;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
@@ -27,9 +28,18 @@ class ProjectController extends Controller
 
     public function store(ProjectRequest $projectrequest)
     {
-        $project = Project::create([$projectrequest->all()]);
+        $data = $projectrequest->all();
+        $data["create_by"] = auth()->user()->id;
+        $project = Project::create($data);
+
+        $project_user = new ProjectUser();
+        $project_user->project_id = $project->id;
+        $project_user->user_id = auth()->user()->id;
+        $project_user->role = 1;
+        $project_user->save();
+
         if($project->fails()){
-            return  $this->commonResponse([],"Project Not Found",400);
+            return  $this->commonResponse($project,"Project Not Found",400);
         }
     return  $this->commonResponse($project,"",200);
     }
